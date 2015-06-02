6fb35fbc5b9d27e4a8faa1dfaa7806a62f8d2f95
yukstar716 <yumatsu.july19920716@gmail.com>
2015-06-02 14:44:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/yuki19920716/namescore/MyForm.java
@@ -0,0 +1,67 @@
+package com.example.yuki19920716.namescore;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MyForm extends ActionBarActivity {
+
+    public final static String EXTRA_MYNAME = "com.example.yuki19920716.namescore.MYNAME";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_form);
+    }
+
+    public void getScore(View v) {
+        EditText myEditText = (EditText) findViewById(R.id.myEditText);
+        String myName = myEditText.getText().toString().trim();
+        if (myName.equals("")){
+            //myEditText.setError("Please Enter Your Name!");
+            //Toast.makeText(this, "Please Enter Your Name!", Toast.LENGTH_LONG).show();
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+            alertDialogBuilder
+                    .setTitle("Error!")
+                    .setMessage("Please Enter Your Name!")
+                    .setPositiveButton("OK", null);
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+
+        } else {
+            Intent intent = new Intent(this, MyResult.class);
+            intent.putExtra(EXTRA_MYNAME, myName);
+            startActivity(intent);
+
+            }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my_form, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/yuki19920716/namescore/MyResult.java
@@ -0,0 +1,56 @@
+package com.example.yuki19920716.namescore;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+public class MyResult extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_result);
+
+        Intent intent = getIntent();
+        String myName = intent.getStringExtra(MyForm.EXTRA_MYNAME);
+        TextView myLabel = (TextView) findViewById(R.id.myLabel);
+        myLabel.setText(myName + "さんの点数は...");
+
+        Random randomGenerator = new Random();
+        int score = randomGenerator.nextInt(101);
+        String result = Integer.toString(score);
+        TextView  myResult = (TextView) findViewById(R.id.myResult);
+        myResult.setText(result + "点!!");
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my_result, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
